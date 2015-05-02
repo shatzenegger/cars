18819eeaa0eabb1636a7492b4148bd765509120b
ericroed <ericroed@hotmail.com>
2015-05-01 21:04:09 -0400

    Added definition to cars

---
+++ lib/src/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.shatzenegger.cars.lib
+{
+    class Car
+    {
+        /*
+         * Model - object
+         * Year - int
+         * Color - enum
+         * Engine - object
+         * Number of Doors - int
+         * Number of Tires - int
+         * Convertible
+         * Height -double
+         * Width- double
+         * Depth - double
+         * Weight - double
+         * Seats - array
+         */
+    }
+}
