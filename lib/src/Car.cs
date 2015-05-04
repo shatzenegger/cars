02f92ea515e3b5a3c0821d8e0197d4c5bd06976e
ericroed <ericroed@hotmail.com>
2015-05-04 15:29:54 -0400

    Defined Objects and Primitives
    Steve, here is a crack at defining primitives and objects.

--- lib/src/Car.cs
+++ lib/src/Car.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace com.shatzenegger.cars.lib
 {
-    class Car
+    class Vehicle
     {
         /*
          * Model - object
@@ -16,7 +16,7 @@ namespace com.shatzenegger.cars.lib
          * Number of Doors - int
          * Number of Tires - int
          * Convertible
-         * Height -double
+         * Height - double
          * Width- double
          * Depth - double
          * Weight - double
@@ -24,6 +24,33 @@ namespace com.shatzenegger.cars.lib
          */
 
         public int Id { get; set; }
-        public Model Model { get; set; }
+        public enum Model { Focus, Fiesta, Fusion, Mustang, Cmax, Taurus, Explorer, F150 }
+        public int Year { get; set; }
+        public enum Color { Black, Red, Blue, Silver, Yellow, Orange, White  }
+        public void Engine { get; set; }
+        public int Number_of_Doors { get; set; }
+        public int Number_of_Tires { get; set; }
+        public bool Convertible { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Weight { get; set; } 
+        public void Seats { get; set; } 
+        public enum Transmission { Automatic, Manual }
+
+    }
+
+    
+
+    class Engine
+    {
+        public int Cylinders { get; set; }
+        public int Horse_Power { get; set; }
+    }
+
+    class Seats
+    {
+        public int number_of_seats { get; set; }
+        public enum seat_style { Captain, Bucket, Bench, Saddle }
     }
 }
